5042d511739a0a045b635c2fea727233acdc3c7f
Jessica Santizo Galicia <js@ciencias.unam.mx>
2023-02-17 09:47:54 -0600

    Programa con un ejemplo de cómo usar print en Python

---
+++ ejemplos_print.py
@@ -0,0 +1,11 @@
+'''
+    Este es un ejemplo para imprimir mensajes en Python
+    Author: Jessica
+'''
+#Este es un comentario de una sola línea
+print('\nEste es un ejemplo de un script en Python')
+print("\nHola Elsa Pato")
+#Podemos dar un salto de línea con \n
+print('\nEstamos muy contentos por que estás aprendiendo a programar en Python \n')
+print('"!!Muchas Felicidades!!"')
+print('\nUn abrazo:)')
