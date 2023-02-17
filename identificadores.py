51215746cf254d55cd49ad9bff41b64b9e91e91a
Jessica Santizo Galicia <js@ciencias.unam.mx>
2023-02-17 09:49:11 -0600

    Se agrega un programa con errores de sintáxis

---
+++ identificadores.py
@@ -0,0 +1,19 @@
+
+'''
+El objetivo de este programa es que el alumno pueda identificar 
+los errores de sintáxis y lógicos
+Ejercicio: Corrige los errores que encuentres y ejecuta tu programa
+Author: Jessica
+'''
+class = "Elsa Pato"
+type(class);
+PI = 3.1416;
+15 = radio;
+area = PI * (radio ** radio);
+mensaje == "Resultado = "
+print('Mensaje 1 \n'+nombre)
+print(mensaje,area)
+print('Mensaje 2 \n')
+print(mensaje + area)
+print('Mensaje 3 \n')
+print('{:.2f}'.format(resultado))
