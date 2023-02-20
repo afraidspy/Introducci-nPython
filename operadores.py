7e3d1a5a09f24444e39d59dde1d45ff933a44c7b
Jessica Santizo Galicia <js@ciencias.unam.mx>
2023-02-20 09:06:03 -0600

    Se agrega programa con ejercicios sobre operadores

---
+++ operadores.py
@@ -0,0 +1,28 @@
+#Operadores aritméticos
+
+resultado =  46 % 9
+print(f'Resultado {resultado}')
+
+resultado =  46 % 9 + 4 * 4 - 2
+print(f'Resultado {resultado}')
+resultado *=2 * 3 + (23 * 3 % 2)
+print(f'Resultado {resultado}')
+
+#Operadores lógicos
+x = 5
+y = 7
+z = 8   
+resultado = x < y and (y < z or x == 5)
+print("Resultado = ", resultado)
+
+#Operadores Binarios
+a = 0b10001
+b = 0b10110
+print ("a = ",a)
+print ("b = " ,  b)
+print("a|b = ", a | b)
+print("a&b = ", a & b)
+print("a^b = ", a ^ b)
+print("a<<2 = ", a << 2)
+#print("a>>2 = ", a >> 2)
+
