202554cd99acdbfab720adae77f5c36842294a38
Jessica Santizo Galicia <js@ciencias.unam.mx>
2023-02-20 08:14:11 -0600

    Se agrega el programa para intercambiar valores

---
+++ intercambio_valores.py
@@ -0,0 +1,19 @@
+if __name__ == 'main':
+    x = 1
+    y = 2
+
+    x += 1
+    y += 1
+
+    '''aux = x
+    x = y
+    y = aux
+    '''
+
+    '''x = y
+    y = x'''
+
+    x , y =  y , x
+
+    print("x: " + str(x))
+    print("y: ",y)
