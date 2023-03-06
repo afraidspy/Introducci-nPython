36dbcc19f363d89179e6bf494433dfd2898b5085
Jessica Santizo Galicia <js@ciencias.unam.mx>
2023-03-06 09:14:32 -0600

    Se agregan ejemplos básicos sobre la estructura de control for

---
+++ estructuras_de_control/estructura_for/for_sintaxis.py
@@ -0,0 +1,104 @@
+
+
+if __name__ == "__main__":
+    
+##    print("Imprimiendo números del 1 al 5")
+##    for i in [1,2,3,4,5]:
+##        print(i)
+
+##    print("Sumar a todos los números de una lista")
+##  
+##    for i in [1,2,3,4,5]:
+##        j += i
+##        print(j)
+
+    print("Imprimiendo si un número es par o impar")
+##    for i in [1,2,3,4,5,6]:
+##        if i % 2 == 0:
+##            print(i,"es par")
+##        else:
+##            print(i,"no es par")
+##            
+##    for i in ["manzana","pera","guayaba"]:
+##        #print(i)
+##        for j in ["rojo","verde","amarillo"]:
+##            print(i,j)
+##
+##    print("Imprimiendo número en el intervalo [1,10)")
+##    for i in range(1,10):
+##        print(i)
+##          
+##    print("Imprimiendo número en el intervalo [0,10)")
+##    for i in range(10):
+##        print(i)
+##       
+##    print("Imprimiendo número en el intervalo [1,10) pero con saltos de 2")
+##    for i in range(1,10,2):
+##        print(i)
+##
+##
+##    print("Imprimiendo cada letra de una cadena.")
+##    for i in "programacion":
+##        print(i)
+##        
+##
+##    print("Imprimiendo carácteres de una cadena")
+##    cadena = "Guido Van Rossum"
+##    for letra in cadena:
+##        print(letra)
+##
+##    print("Analizando el uso del else en un for ")
+    cadena = "Hola, mundo!"
+##
+##    for letra in cadena:
+##        print(letra)
+##        if letra == " ":
+##            print("La cadena contiene un espacio en blanco.")
+##            break
+##    else:
+##        print("La cadena no contiene espacios en blanco.")
+##
+##
+##    for i in [1,2,3,4,5]:
+##        if i ==3:
+##            continue
+##        print(i)
+##        
+##       
+    for i in [1,2,3,4,5]:
+        if i ==4:
+            continue
+        print(i)
+##
+
+
+    """
+        Escribe una función que reciba un número e imprima la tabla de multiplicar de ese número
+    """
+        
+
+   
+
+    #Sintaxis java 
+    #for (int i=1;i<=5,i=i+1){
+    #   print(i)
+    #}
+ 
+    
+            
+            
+    
+            
+            
+            
+    
+        
+        
+   
+        
+  
+
+
+   
+  
+
