256ac72840a7d45152e5dc97c99d60a6ed0f9fc9
Jessica Santizo Galicia <js@ciencias.unam.mx>
2023-05-03 09:27:18 -0600

    Se agrega solución de los ejercicios de listas por comprensión

---
+++ listas_comprension/ejercicios.py
@@ -0,0 +1,31 @@
+'''
+Ejercicios
+
+Objetivo: El alumno practicará la sintáxis  de listas por comprensión
+
+Instrucciones: Resuelve los siguientes ejercicios, puedes entregar un archivo ed Python ó compartir el link de un notebook de Google Colab
+ó entregar tus respuestas en una hoja antes de que termine la clase
+'''
+
+#1.Dada una lista llamada numeros, cuál sería la instrucción para que se generé la siguiente lista [(2, 4), (4, 8), (8, 16), (10, 20)]
+
+numeros = [2,4,8,10]
+
+#2. Dada una cadena, cuenta cuantos espacios tiene la cadena usando una lista por comprensión
+cadena = "Usar listas por comprensión es más eficiente "
+espacios = [caracter for caracter in cadena if caracter == ' ' ]
+
+#3. De la cadena anterior, elimina todas las vocales 
+#Hint: Utiliza la función join() 
+
+#4. Indica que lista genera la siguiente instrucción. Explica tu resultado
+
+lista = [(x, y, z) for x in range(1, 5) for y in 'ab' for z in range(2, 11, 2) if x%2 == 0 if (x*z) >= 1]
+
+#5. Indica que imprime la lista genera el siguiente código, posteriormente crea el código equivalente haciendo uso de un for tradicional
+resultados = ["FizzBuzz" if i % 3 == 0 and i % 5 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else i for i in range(1, 11)]
+print("Resultado usando listas por comprension")
+print(resultados)
+
+
+
---
+++ matrices/matrices2.py
@@ -0,0 +1,85 @@
+class Matriz:
+    def __init__(self, filas, columnas):
+        self.__filas = filas
+        self.__columnas = columnas
+        self.__matriz = []
+
+        self.__llenar()
+        print(self.__matriz)
+
+
+    def __llenar(self):
+        for i in range(self.__filas):
+            fila = []
+            for j in range (self.__columnas):
+                 fila.append(0.0)
+            self.__matriz.append(fila)
+
+    def llenar_valores_usuario(self):
+        for i in range(self.__filas):
+            for j in range(self.__columnas):
+                self.__matriz[i][j] = float(input(f"Ingrese el valor para la posición [{i}][{j}]: "))
+
+    def sumar_matrices(self, otra_matriz):
+        if self.__filas != otra_matriz.__filas or self.__columnas != otra_matriz.__columnas:
+            print("Las matrices deben tener las mismas dimensiones para poder sumarlas.")
+        resultado = Matriz(self.__filas, self.__columnas)
+        for i in range(self.__filas):
+            for j in range(self.__columnas):
+                resultado.__matriz[i][j] = self.__matriz[i][j] + otra_matriz.__matriz[i][j]
+        return resultado
+   
+    def obtener_transpuesta(self):
+        transpuesta = Matriz(self.__columnas, self.__filas)
+        for i in range(self.__columnas):
+            for j in range(self.__filas):
+                transpuesta.__matriz[i][j] = self.__matriz[j][i]
+        return transpuesta
+
+    def es_simetrica(self):
+        if self.__filas != self.__columnas:
+            return False
+        for i in range(self.__filas):
+            for j in range(self.__columnas):
+                if self.__matriz[i][j] != self.__matriz[j][i]:
+                    return False
+        return True
+   
+    def multiplicar(self, matriz_b):
+        if self.__columnas != matriz_b.__filas:
+            print("No se pueden multiplicar las matrices. El número de columnas de A no es igual al número de filas de B.")
+            return None
+
+        matriz_c = Matriz(self.__filas, matriz_b.__columnas)
+
+        for i in range(self.__filas):
+            for j in range(matriz_b.__columnas):
+                suma = 0
+                for k in range(self.__columnas):
+                    producto = (self.__matriz[i][k] * matriz_b.__matriz[k][j])
+                    suma += producto
+                    print(f"i={i}, j={j}, k={k}, producto={producto} suma={suma}")
+                matriz_c.__matriz[i][j] = suma
+
+        return matriz_c
+
+    def __str__(self):
+        matriz_str = ""
+        for i in range(self.__filas):
+            for j in range(self.__columnas):
+                matriz_str += str(self.__matriz[i][j]) + " "
+            matriz_str += "\n"
+           
+        return matriz_str
+
+
+matriz = Matriz(2,2)
+matriz.llenar_valores_usuario()
+print(matriz)
+matriz2 = Matriz(2,2)
+matriz2.llenar_valores_usuario()
+print(matriz2)
+
+
+print(matriz2.multiplicar(matriz2))
+
